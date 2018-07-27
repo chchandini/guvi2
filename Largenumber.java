b03a012e55d2e7fd817463c3b4b2dd7a29452888
chchandini <41769685+chchandini@users.noreply.github.com>
2018-07-26 23:45:36 -0700

    Create Largenumber.java

---
+++ Largenumber.java
@@ -0,0 +1,35 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter a number");
+		int sc=sc.nextInt();
+		System.out.println("Enter a value");
+		int b=sc.nextInt;
+		System.out.printl("enter c value");
+		int c=sc.nextInt();
+		if(a>b)
+	{
+		System.out.println("a+is integer");
+	}
+	else if(a>c)
+	{
+		System.out.println(a+is larger");
+	}
+	else
+	{
+		System.out.println(b+"is  larger");
+	}
+	
+	}
+	
+
+}
