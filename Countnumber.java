d99b24913a52eb61ef6bce479de88d027ed2197d
chchandini <41769685+chchandini@users.noreply.github.com>
2018-07-26 23:57:02 -0700

    Create Countnumber.java

---
+++ Countnumber.java
@@ -0,0 +1,24 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=neww Scanner(System.in");
+		System.out.println("Enter a number");
+		int a=sc.nextInt();
+		int count=0;
+		if(a!=0)
+		{
+				a=a/10;
+				++count;
+		}
+		System.out.println("count");
+		}
+	}
+}
