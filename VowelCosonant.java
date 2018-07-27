64a09af6fa9ececd76dac691571b6d400a8ae15a
chchandini <41769685+chchandini@users.noreply.github.com>
2018-07-27 00:26:53 -0700

    Create VowelCosonant.java

---
+++ VowelCosonant.java
@@ -0,0 +1,28 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		System.out.println("System.in");
+		char ch=sc.nrxt().charAt(0);
+		if(ch='a'||'e'||'i'||'o'||'u')
+		{
+			System.out.printl("ch+"vowel");
+		}
+		else
+		System.out.println(""vowel");
+	}
+	else
+	{
+		System.out.println("consonant");
+	}
+}
+}
+	
