1665b66683a950cede95d200d04e967bb1b5535f
chchandini <41769685+chchandini@users.noreply.github.com>
2018-07-27 08:25:22 -0700

    Create VowelorConsonant.java

---
+++ VowelorConsonant.java
@@ -0,0 +1,26 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{ch=='e'||ch=='i'||ch=='o||ch='u'||ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u')ch<='z'&&
+	{
+		System.out.println("vowel");
+	}
+	else if(ch>='a'||ch<='z'&&ch>='A'||ch<='p')
+	{
+		System.out.println("consonant");
+	}
+	else
+	{
+		System.out.println("Invalid");
+	}
+	}
+	}
+	}
+}
