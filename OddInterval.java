6e36fb0b22c9ecc74ace3845fb3700180330765d
chchandini <41769685+chchandini@users.noreply.github.com>
2018-07-28 07:55:40 -0700

    Create OddInterval.java

---
+++ OddInterval.java
@@ -0,0 +1,31 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		public class ListOddNumbers {
+ 
+	public static void main(String[] args) {
+		
+		//define the limit
+		int limit = 50;
+		
+		System.out.println("Printing Odd numbers between 1 and " + limit);
+		
+		for(int i=1; i <= limit; i++){
+			
+			//if the number is not divisible by 2 then it is odd
+			if( i % 2 != 0){
+				System.out.print(i + " ");
+			}
+		}
+	}
+
+	}
+}
