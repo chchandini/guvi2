2d50fceda0ff7fbc1c854dba3c3b5638ca9da577
chchandini <41769685+chchandini@users.noreply.github.com>
2018-07-28 08:04:47 -0700

    Create EvenInterval.java

---
+++ EvenInterval.java
@@ -0,0 +1,30 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		public class ListevenNumbers {
+ 
+	public static void main(String[] args) {
+		
+		//define the limit
+		int limit = 50;
+		
+		System.out.println("Printing even numbers between 1 and " + limit);
+		
+		for(int i=1; i <= limit; i++){
+			
+			if( i % 2 != 0){
+				System.out.print(i + " ");
+			}
+		}
+	}
+
+	}
+}
