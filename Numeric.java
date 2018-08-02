a4fa51b3169e9f9701c8a8f4ef61a15315453cf0
chchandini <41769685+chchandini@users.noreply.github.com>
2018-08-02 01:28:11 -0700

    Create Numeric.java

---
+++ Numeric.java
@@ -0,0 +1,21 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	
+	{
+		public static boolean isNumeric(String str);
+{
+  NumberFormat formatter = NumberFormat.getInstance();
+  ParsePosition pos = new ParsePosition(0);
+  formatter.parse(str, pos);
+  return str.length() == pos.getIndex();
+}
+
+	}
+}
