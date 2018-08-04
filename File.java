6138b6b3d943e35447d45e8062d2bbdca01d85eb
chchandini <41769685+chchandini@users.noreply.github.com>
2018-08-04 08:10:17 -0700

    Create File.java

---
+++ File.java
@@ -0,0 +1,30 @@
+Scanner sc=new Scanner(System.in);
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+			int[a] a=new int[10];
+				for(int i=0;i<10;i++)
+			{
+				a[i]=sc.nextInt();
+			}
+				for(int i=0;i<10;i++)
+				{
+					if(a[j]>a[j+1]
+					{
+						int tmp=a[j];
+						a[j]=a[j+1];
+						a[j+1]=tmp;
+					}
+				}
+			}		
+	}
+}
