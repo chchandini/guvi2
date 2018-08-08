bbd0f2270cdec7d0a3264cee6be906976b05665d
chchandini <41769685+chchandini@users.noreply.github.com>
2018-08-08 08:17:05 -0700

    Create Character.java

---
+++ Character.java
@@ -0,0 +1,33 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		public class WordCount {
+		
+      static int wordCount(String string)  
+        {  
+          int count=0;  
+      
+            char ch[]= new char[string.length()];     
+            for(int i=0;i<string.length();i++)  
+            {  
+                ch[i]= string.charAt(i);  
+                if( ((i>0)&&(ch[i]!=' ')&&(ch[i-1]==' ')) || ((ch[0]!=' ')&&(i==0)) )  
+                    count++;  
+            }  
+            return count;  
+        }  
+      public static void main(String[] args) {  
+          String string ="    India Is My Country";  
+         System.out.println(wordcount(string) + " words.");   
+    }  
+}  
+	}
+}
