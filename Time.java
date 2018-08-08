492aae98a4b8334d4bc2e80bf60014bbca200807
chchandini <41769685+chchandini@users.noreply.github.com>
2018-08-07 23:03:40 -0700

    Create Time.java

---
+++ Time.java
@@ -0,0 +1,26 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		public class someClass
+{
+   public static void main(String[] args) // your app start point
+   {
+       long start = java.util.Calendar.getInstance().getTimeInMillis();
+
+       ... your stuff ...
+
+       long end = java.util.Calendar.getInstance().getTimeInMillis();
+       System.out.println("it took this long to complete this stuff: " + (end - start) + "ms");
+   }
+}
+
+	}
+}
