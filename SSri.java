d2109ff3067cff8661e35c7700024bc96dc3f63f
chchandini <41769685+chchandini@users.noreply.github.com>
2018-08-08 23:04:16 -0700

    Update SSri.java

--- SSri.java
+++ SSri.java
@@ -11,9 +11,9 @@ class Ideone
 	{
 		Scanner sc=new Scanner(System.in);
 		String str=sc.nextLine();
-		for(int i=str.length()-1;i>=0;i--)
+		for(int j=str.length()-1;j>=0;j--)
 		{
-			System.out.print(str.charAt(i));
+			System.out.print(str.charAt(j));
 		}
 	}
 }
