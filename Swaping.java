43408440212eb82abceb9713802e5be83ef85e1c
chchandini <41769685+chchandini@users.noreply.github.com>
2018-08-09 23:47:18 -0700

    Create Swaping.java

---
+++ Swaping.java
@@ -0,0 +1,34 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	
+
+	public static void main(String[] args)
+	{
+		Scanner sc=new Scanner(System.in);
+		        float first = 1.20f, second = 2.45f;
+
+        System.out.println("--Before swap--");
+        System.out.println("First number = " + first);
+        System.out.println("Second number = " + second);
+
+                float temporary = first;
+
+                first = second;
+
+        // Value of temporary (which contains the initial value of first) is assigned to second
+        second = temporary;
+
+        System.out.println("--After swap--");
+        System.out.println("First number = " + first);
+        System.out.println("Second number = " + second);
+    }
+}
+
+	
