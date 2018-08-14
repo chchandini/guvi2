40be1851d91dcc31472370ee461d67efc5326e4e
chchandini <41769685+chchandini@users.noreply.github.com>
2018-08-13 21:42:42 -0700

    Create Sum5.java

---
+++ Sum5.java
@@ -0,0 +1,21 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		int a=sc.nextInt();
+		int b=sc.nextInt();
+		int c=sc.nextInt();
+		int d=sc.nextInt();
+		int sum;
+		sum=a+b+c+d;
+		System.out.println(sum);
+	}
+}
+	
