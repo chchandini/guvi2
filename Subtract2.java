334008b711b7b55a8501b0ffb46596a0f1ce3d3f
chchandini <41769685+chchandini@users.noreply.github.com>
2018-08-16 06:57:46 -0700

    Create Subtract2.java

---
+++ Subtract2.java
@@ -0,0 +1,21 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int k1=sc.nextInt();
+		int m1=sc.nextInt();
+		int k2=sc.nextInt();
+		int m2=sc.nextInt();
+		int k3=k1-k2;
+		int m3=m1-m2;
+		System.out.println(k3+" "+m3);
+	}
+}
