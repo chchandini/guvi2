7b34b5862de20cc174690e45a6be902353b3971a
chchandini <41769685+chchandini@users.noreply.github.com>
2018-08-22 21:48:45 -0700

    Create Special character.java

---
+++ Special character.java
@@ -0,0 +1,33 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		String str;
+		int i,j,count=0;
+		Scanner sc=new Scanner(System.in);
+		str=sc.nextLine();
+		for(int i=0;i<str.length();i++)
+			for(j=0;j<special length();j++)
+			{
+				if(str.charAt(i)==special.charAt(j))
+				{
+					count++;
+				}
+			}
+	}
+	System.out.println(" "+count);
+	}
+public static void main(String[] args)
+{
+		fun(" !#$%&'()*+,-./:;<=>?@[\\]^{1}~");
+
+	}
+
+}
