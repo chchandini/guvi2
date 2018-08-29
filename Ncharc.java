768cb512387aacf7245e5d70c40bf07731fc9220
chchandini <41769685+chchandini@users.noreply.github.com>
2018-08-28 21:45:42 -0700

    Create Ncharc.java

---
+++ Ncharc.java
@@ -0,0 +1,21 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		String str = "chandini";
+      int n = 5;
+      int initial = str.length()-5;
+      for(int i=initial; i<str.length(); i++) {
+         System.out.println(str.charAt(i));
+      }
+   }
+
+	
+}
