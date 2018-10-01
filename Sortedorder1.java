4a0597cdf80b39166b9e715aa95fba83b753558f
chchandini <41769685+chchandini@users.noreply.github.com>
2018-10-01 06:06:54 -0700

    Create Sortedorder1.java

---
+++ Sortedorder1.java
@@ -0,0 +1,24 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+        int[] arr1 = {1,2,3,4};
+        int[] arr2 = {1,2,3,4,5};
+        for(int i=0;i<arr1.length;i++){
+            for(int j=0;j<arr2.length;j++){
+                if(arr1[i]==arr2[j]){
+                    System.out.println(arr1[i]);
+                }
+            }
+		}
+	}
+}
+        
+    
